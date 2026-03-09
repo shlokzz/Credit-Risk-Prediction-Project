0cb1748c08d175f5f40ec1a78e6f6f5b29369785
shlokzz <shlok.pd.joshi@gmail.com>
2026-03-09 19:48:48 +0545

    Load the saved models

--- apps/app.py
+++ apps/app.py
@@ -1,3 +1,19 @@
 import numpy as np
 import streamlit as st
 import pickle
+
+# load the saved trained model
+with open("models/trained_model.sav", "rb") as f:
+    loaded_trained_model = pickle.load(f)
+
+# loading the saved scaler
+with open("models/labelencoder.sav", "rb") as f:
+    loaded_trained_model = pickle.load(f)
+
+# loading the saved scaler
+with open("models/standardscaler.sav", "rb") as f:
+    loaded_scaler = pickle.load(f)
+
+# loading the saved scaler
+with open("models/one_hot_encoder.sav", "rb") as f:
+    loaded_one_hot_encoder = pickle.load(f)
