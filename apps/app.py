4dbf49a587b962ce6474da81c1b1cebc802e7337
shlokzz <shlok.pd.joshi@gmail.com>
2026-03-09 19:54:20 +0545

    refactor: fix the loaded_trained_model to loaded_label_encoder_model and also import pandas

--- apps/app.py
+++ apps/app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import streamlit as st
 import pickle
+import pandas as pd
 
 # load the saved trained model
 with open("models/trained_model.sav", "rb") as f:
@@ -8,7 +9,7 @@ with open("models/trained_model.sav", "rb") as f:
 
 # loading the saved scaler
 with open("models/labelencoder.sav", "rb") as f:
-    loaded_trained_model = pickle.load(f)
+    loaded_label_encoder_model = pickle.load(f)
 
 # loading the saved scaler
 with open("models/standardscaler.sav", "rb") as f:
